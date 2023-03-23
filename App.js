6c2c3d9ca69e1853cadf2e116f195ca142309598
gguilhermelopes <guilhermelopes275@gmail.com>
2023-03-23 14:15:34 -0300

    feat: create drawer navigation babel configs

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
+import "react-native-gesture-handler";
+import { useCallback, useEffect } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import CategoriesScreen from "./screens/CategoriesScreen";
-import { useCallback, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsListScreen from "./screens/MealsListScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   const [fontsLoaded] = useFonts({
